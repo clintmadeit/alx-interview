c2006390b354db3ff98d76b6e7958cde28021dff
clintmadeit <clintonbrirdley@gmail.com>
2023-07-10 09:15:39 +0300

    minOperations

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print(f"Min # of operations to reach {n} char: {minOperations(n)}")
+
+n = 12
+print(f"Min # of operations to reach {n} char: {minOperations(n)}")

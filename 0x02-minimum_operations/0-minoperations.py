d9a27fecf55fabfafadd07be7636ec1beb01f924
clintmadeit <clintonbrirdley@gmail.com>
2023-07-13 22:30:56 +0300

    minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+
+
 """
-minimum operations
+Calculates the minimum operations
 """
+
+
 def minOperations(n):
-    """minOperations"""
-    if n <= 1:
-        return 0
-    for i in range(2, n + 1):
-        if n % i == 0:
-            return minOperations(n // i) + i
+    answer = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            answer += x
+            n /= x
+        x += 1
+    return answer
